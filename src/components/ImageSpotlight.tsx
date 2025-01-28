0f967cc52f9dacb723f553506ccfe7c88d15782f
Scott Langridge <scott@derossi.consulting>
2025-01-28 23:53:36 +0000

    Enlarge spotlight image

--- src/components/ImageSpotlight.tsx
+++ src/components/ImageSpotlight.tsx
@@ -7,20 +7,18 @@ export default function ImageSpotlight({open, setOpen, imageNode}) {
     const image = imageNode ? getImage(imageNode.childImageSharp.gatsbyImageData) : null;
 
     return (
-        <Flex position="fixed" top="0" left="0" width="100vw" height="100vh" bg="rgba(0, 0, 0, 0.75)" zIndex="overlay" hidden={!open} onClick={() => {setOpen(false)}}>
-            <AbsoluteCenter>
+        <Flex position="fixed" top="0" left="0" width="100vw" height="100vh" bg="rgba(0, 0, 0, 0.75)" zIndex="overlay" justifyContent="center" alignItems="center" hidden={!open} onClick={() => {setOpen(false)}}>
                     <GatsbyImage
                         image={image}
                         alt="img"
                         objectFit="contain"
                         style={{
-                            maxWidth: "90vh",
+                            maxWidth: "90vw",
                             maxHeight: "90vh",
                             width: "100%",
                             height: "100%",
                         }}
                     />
-            </AbsoluteCenter>
         </Flex>
     )
 }
